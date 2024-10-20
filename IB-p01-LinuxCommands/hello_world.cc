9f0733a046636d2c3fa1aac8728799a2fd5a85da
IsmaelOuldCH <alu0101721415@ull.edu.es>
2024-10-20 18:59:48 +0000

    IBP01

---
+++ IB-p01-LinuxCommands/hello_world.cc
@@ -0,0 +1,21 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Informática Básica
+ *
+ * @author Ismael Ould Chalague Hernandez
+ * @date 26 Sep 2024
+ * @brief 'Hello World' first programm
+ *
+ * @see http://www.cplusplus.com/doc/tutorial/program_structure/
+ * @see http://cpp.sh/2dd
+ */
+
+#include <iostream>
+
+ int main() {
+  // Is this your first C++ program ?
+  std::cout << "Hello World!" << std::endl;
+  return 0;
+}
